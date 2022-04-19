94ca4e6b59fffc377cb0b9d5dab5f1cf57d7a786
misterjcs <443279918@qq.com>
2022-04-19 22:56:14 +0800

    Update fan_ctrl.sh

--- fan_ctrl.sh
+++ fan_ctrl.sh
@@ -4,9 +4,9 @@
 temp=$(cat /sys/devices/virtual/thermal/thermal_zone0/temp)
 echo $temp
 #判断温度(高于50度开启风扇，降到45度后关闭，避免停停转转)
-if [ temp -gt 50000 ];then
+if [ $temp -gt 50000 ];then
      echo 1 > /sys/class/gpio/gpio70/value
-elif [ temp -lt 45000 ];then
+elif [ $temp -lt 45000 ];then
      echo 0 > /sys/class/gpio/gpio70/value
 else
 	:
